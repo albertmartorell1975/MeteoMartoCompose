78de8cb8f79badf0322d35cb3891120f5cb5df04
AlbertMartorell <albert.martorell@opentrends.net>
2025-09-09 13:29:01 +0200

    Extracts UI logic from HomeScreen to AppState

--- app/src/main/java/com/martorell/albert/meteomartocompose/ui/AppState.kt
+++ app/src/main/java/com/martorell/albert/meteomartocompose/ui/AppState.kt
@@ -2,9 +2,17 @@ package com.martorell.albert.meteomartocompose.ui
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -15,16 +23,26 @@ import com.martorell.albert.meteomartocompose.R
 import com.martorell.albert.meteomartocompose.ui.navigation.DashboardScreens
 import com.martorell.albert.meteomartocompose.ui.navigation.navigatePoppingUpToStartDestination
 import com.martorell.albert.meteomartocompose.ui.navigation.shared.TopLevelRoute
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun rememberAppState(
     navController: NavHostController = rememberNavController(),
-): AppState = remember(navController) {
-    AppState(navController)
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+): AppState = remember(
+    navController,
+    key2 = coroutineScope
+) {
+    AppState(
+        navController = navController,
+        coroutineScope = coroutineScope
+    )
 }
 
 class AppState(
     private val navController: NavHostController,
+    private val coroutineScope: CoroutineScope
 ) {
 
     companion object {
@@ -80,8 +98,21 @@ class AppState(
 
         }
 
-    fun onNavItemClick(screen: DashboardScreens) {
-        navController.navigatePoppingUpToStartDestination(screen)
+
+    // This state variable directly controls whether the FAB should be part of the composition. It's initialized to false (hidden by default).
+    var isFabVisible by mutableStateOf(false)
+        private set
+
+    fun updateIsFabVisible(newValue: Boolean) {
+        isFabVisible = newValue
+    }
+
+    var userClickedOnFab by mutableIntStateOf(0)
+        private set
+
+    fun updateOnFabClick() {
+        userClickedOnFab =
+            userClickedOnFab + 1 // This will now trigger recomposition for composables reading it
     }
 
     @Composable
@@ -90,6 +121,39 @@ class AppState(
         val navBackStackEntry = navController.currentBackStackEntryAsState().value
         val currentDestination = navBackStackEntry?.destination
         return currentDestination?.hierarchy?.any { it.hasRoute(screen::class) } == true
+
+    }
+
+    @Composable
+    fun fabIcon(isCityFavoriteAction: suspend () -> Boolean): ImageVector? {
+
+        // According to if the just loaded city is favorite or not, we display the proper icon
+        val icon by produceState<ImageVector?>(
+            initialValue = null,
+            key1 = userClickedOnFab
+        ) {
+
+            value = if (isCityFavoriteAction.invoke()) {
+                Icons.Default.Favorite
+            } else
+                Icons.Default.FavoriteBorder
+        }
+
+        return icon
+
+    }
+
+    // actions
+    fun onNavItemClick(screen: DashboardScreens) {
+
+        navController.navigatePoppingUpToStartDestination(screen)
+
+    }
+
+    fun onFabClick(onFabAction: () -> Unit) {
+
+        coroutineScope.launch { onFabAction.invoke() }
+        updateOnFabClick()
 
     }
 
--- app/src/main/java/com/martorell/albert/meteomartocompose/ui/screens/dashboard/HomeScreen.kt
+++ app/src/main/java/com/martorell/albert/meteomartocompose/ui/screens/dashboard/HomeScreen.kt
@@ -1,9 +1,6 @@
 package com.martorell.albert.meteomartocompose.ui.screens.dashboard
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -11,15 +8,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -33,7 +22,6 @@ import com.martorell.albert.meteomartocompose.ui.navigation.shared.NavigationBar
 import com.martorell.albert.meteomartocompose.ui.navigation.shared.TopAppBarCustom
 import com.martorell.albert.meteomartocompose.ui.rememberAppState
 import com.martorell.albert.meteomartocompose.ui.screens.city.CityWeatherViewModel
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,10 +33,6 @@ fun HomeScreen(
 
     val scrollState = rememberTopAppBarState()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(scrollState)
-    val coroutineScope = rememberCoroutineScope()
-    // State to hold the FAB's visibility.
-    // This state variable directly controls whether the FAB should be part of the composition. It's initialized to false (hidden by default).
-    var isFabVisible by remember { mutableStateOf(false) }
 
     MeteoMartoComposeLayout {
 
@@ -70,28 +54,17 @@ fun HomeScreen(
             },
             floatingActionButton = {
 
-                if (appState.showFavoriteButton && isFabVisible) {
-
-                    // According to if the just loaded city is favorite or not, we display the proper icon
-                    var userClickedOnFab by remember { mutableIntStateOf(0) }
-
-                    val icon by produceState<ImageVector?>(
-                        initialValue = null,
-                        key1 = userClickedOnFab
-                    ) {
-                        value = if (cityWeatherViewModel::isCityFavorite.invoke()) {
-                            Icons.Default.Favorite
-                        } else
-                            Icons.Default.FavoriteBorder
-                    }
+                if (appState.showFavoriteButton && appState.isFabVisible) {
 
                     FloatingActionButton(onClick = {
-                        coroutineScope.launch { cityWeatherViewModel::onFavoriteClicked.invoke() }
-                        userClickedOnFab += 1
+                        appState.onFabClick { cityWeatherViewModel::onFavoriteClicked.invoke() }
                     }) {
+
+                        val icon =
+                            appState.fabIcon(isCityFavoriteAction = { cityWeatherViewModel::isCityFavorite.invoke() })
                         icon?.run {
                             Icon(
-                                imageVector = this,
+                                imageVector = icon,
                                 contentDescription = stringResource(R.string.favorite)
                             )
                         }
@@ -109,7 +82,7 @@ fun HomeScreen(
                 navController = navController,
                 modifier = Modifier.padding(innerPadding),
                 setFabVisibility = { newVisibility ->
-                    isFabVisible = newVisibility
+                    appState.updateIsFabVisible(newVisibility)
                 })
 
         }
